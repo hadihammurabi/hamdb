51109498f13699e2fb61c8e34cf2396b2234c0dd
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2017-12-24 14:21:28 +0700

    Add <Func> :: Create Database

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Starting Project")
+#!/usr/bin/env node
+
+const cli = require('commander')
+const env = require('./environment')
+const db  = require('./lib/database')
+
+cli
+    .version('Hambase@'+env.version)
+    .description('Realtime NoSQL Database')
+
+cli
+    .command('new <database>')
+    .description('Create new database file')
+    .action((database)=>{
+        db.create(database)
+    })
+
+cli.parse(process.argv)
